38bebc0e0919cd48b1fb2eebf4c7e5a8216c6b86
vkhinvasara <vaibhavkhinvasara@gmail.com>
2024-05-04 21:42:28 +0530

    Project 1 done

--- project-1/src/main.rs
+++ project-1/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::*;
+
+#[get("/")]
+async fn say_hello() -> impl Responder{
+	HttpResponse::Ok().body("Hello world!")
+}
+
+async fn say_juvinile_stuff() -> impl Responder{
+	HttpResponse::Ok().body("I am a child")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
+	HttpServer::new(||{
+		App::new()
+			.service(say_hello)
+			.route("/child", web::get().to(say_juvinile_stuff))
+	}).bind(("127.0.0.1", 8080))? 
+	.run()
+	.await
 }
