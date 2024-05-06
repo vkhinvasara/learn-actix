d505f2342f51db0de6dc6658554c039ff65a7353
vkhinvasara <vaibhavkhinvasara@gmail.com>
2024-05-07 02:09:59 +0530

    Project 3 done

--- project-3/src/main.rs
+++ project-3/src/main.rs
@@ -1,8 +1,8 @@
 use std::collections::HashMap;
 
-use actix_web::{dev::Path, get, post, web::{self, get}, App, HttpResponse, HttpServer, Responder};
+use actix_web::{dev::Path, get, post, delete, put, web::{self, get}, App, HttpResponse, HttpServer, Responder};
 use rusoto_core::Region;
-use rusoto_dynamodb::{AttributeValue, DynamoDb, DynamoDbClient, GetItemInput, ListTablesInput, PutItemInput};
+use rusoto_dynamodb::{AttributeValue, DynamoDb, DynamoDbClient, GetItemInput, DeleteItemInput, PutItemInput};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
@@ -139,12 +139,104 @@ async fn get_customer(id: web::Path<String>)-> impl Responder{
     }
 }
 
+#[delete("/delete_customer/{id}")]
+async fn delete_customer(id: web::Path<String>)-> impl Responder{
+    let client = DynamoDbClient::new(Region::ApSouth1);
+    let delete_item_input = DeleteItemInput{
+        key: {
+            let mut key = HashMap::new();
+            key.insert("customer_key".to_string(), AttributeValue{
+                s: Some(id.to_string()),
+                ..Default::default()
+            });
+            key
+        },
+        table_name: "Customer".to_string(),
+        ..Default::default()
+    };
+    match client.delete_item(delete_item_input).await{
+        Ok(_) => HttpResponse::Ok().body("Customer deleted successfully"),
+        Err(error) => {
+            println!("Error: {:?}", error);
+            HttpResponse::InternalServerError().body("Something went wrong")
+        }
+    }
+}
+
+#[put("/update_customer")]
+
+async fn update_customer(customer: web::Json<Customer>)-> impl Responder{
+    let client = DynamoDbClient::new(Region::ApSouth1);
+    let customer = customer.into_inner();
+    if customer.id.is_empty() 
+    && customer.name.is_empty() 
+    && customer.age == 0
+    && customer.active == false{
+       return  HttpResponse::BadRequest().body("Id is required");
+    }
+
+    let mut item = HashMap::new();
+    item.insert("customer_key".to_string(), AttributeValue{
+        s: Some(customer.id.to_string()),
+        ..Default::default()
+    });
+    item.insert("name".to_string(), AttributeValue{
+        s: Some(customer.name),
+        ..Default::default()
+    });
+    item.insert("age".to_string(), AttributeValue{
+        n: Some(customer.age.to_string()),
+        ..Default::default()
+    });
+    item.insert("active".to_string(), AttributeValue{
+        bool: Some(customer.active),
+        ..Default::default()
+    });
+    item.insert("address".to_string(),AttributeValue{
+        m: Some({
+            let mut address = HashMap::new();
+            address.insert("street".to_string(), AttributeValue{
+                s: Some(customer.address.street),
+                ..Default::default()
+            });
+            address.insert("city".to_string(), AttributeValue{
+                s: Some(customer.address.city),
+                ..Default::default()
+            });
+            address.insert("state".to_string(), AttributeValue{
+                s: Some(customer.address.state),
+                ..Default::default()
+            });
+            address.insert("zip".to_string(), AttributeValue{
+                s: Some(customer.address.zip),
+                ..Default::default()
+            });
+            address
+        }),
+        ..Default::default()
+    });
+    let put_item_input = PutItemInput{
+        item: item,
+        table_name: "Customer".to_string(),
+        ..Default::default()
+    };
+    match client.put_item(put_item_input).await{
+        Ok(_) => HttpResponse::Ok().body("Customer updated successfully"),
+        Err(error) => {
+            println!("Error: {:?}", error);
+            HttpResponse::InternalServerError().body("Something went wrong")
+        }
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()>{
     HttpServer::new(||{
         App::new()
             .service(register_customer)
             .service(get_customer)
+            .service(delete_customer)
+            .service(update_customer)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
