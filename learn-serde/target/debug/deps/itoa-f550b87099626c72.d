b6caf778abeeab98819f6c98f25bd2b88214a126
Vaibhav Khinvasara <89930190+vkhinvasara@users.noreply.github.com>
2024-06-16 21:39:00 +0530

    Delete learn-serde/target directory

--- learn-serde/target/debug/deps/itoa-f550b87099626c72.d
+++
@@ -1,6 +0,0 @@
-C:\Users\vaibh\learn-actix\learn-serde\target\debug\deps\libitoa-f550b87099626c72.rmeta: C:\Users\vaibh\.cargo\registry\src\index.crates.io-6f17d22bba15001f\itoa-1.0.11\src\lib.rs C:\Users\vaibh\.cargo\registry\src\index.crates.io-6f17d22bba15001f\itoa-1.0.11\src\udiv128.rs
-
-C:\Users\vaibh\learn-actix\learn-serde\target\debug\deps\itoa-f550b87099626c72.d: C:\Users\vaibh\.cargo\registry\src\index.crates.io-6f17d22bba15001f\itoa-1.0.11\src\lib.rs C:\Users\vaibh\.cargo\registry\src\index.crates.io-6f17d22bba15001f\itoa-1.0.11\src\udiv128.rs
-
-C:\Users\vaibh\.cargo\registry\src\index.crates.io-6f17d22bba15001f\itoa-1.0.11\src\lib.rs:
-C:\Users\vaibh\.cargo\registry\src\index.crates.io-6f17d22bba15001f\itoa-1.0.11\src\udiv128.rs:
--- learn-serde/target/debug/deps/learn_serde-de5029d5fcbe4694.d
+++
@@ -1,5 +0,0 @@
-C:\Users\vaibh\learn-actix\learn-serde\target\debug\deps\liblearn_serde-de5029d5fcbe4694.rmeta: src\main.rs
-
-C:\Users\vaibh\learn-actix\learn-serde\target\debug\deps\learn_serde-de5029d5fcbe4694.d: src\main.rs
-
-src\main.rs:
