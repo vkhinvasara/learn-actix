f1d01f2d7bc8a91e48cd6c61574bfc9011c61821
vkhinvasara <vaibhavkhinvasara@gmail.com>
2024-06-21 19:19:17 +0530

    minor changes

--- project-5/src/start_connection.rs
+++ project-5/src/start_connection.rs
@@ -1,14 +1,15 @@
 use crate::ws::WsConn;
 use crate::lobby::Lobby;
 use actix::Addr;
-use actix_web::{web, Error, HttpRequest, HttpResponse};
+use actix_web::{get, web, HttpRequest};
 use actix_web_actors::ws;
 use uuid::Uuid;
+use actix_web::Responder;
 
 
-pub async fn start_connection(req: HttpRequest, stream: web::Payload,`` data: web::Data<Addr<Lobby>>) -> Result<HttpResponse, Error> {
-	let room = req.match_info().get("room").unwrap();
-	let room = Uuid::parse_str(room).unwrap();
-	let resp = ws::start(WsConn::new(room, data.get_ref().clone()), &req, stream);
-	resp
+#[get("/{group_id}")]
+pub async fn start_connection(req: HttpRequest, stream: web::Payload, group_id: web::Path<Uuid>,data: web::Data<Addr<Lobby>>) -> impl Responder{
+	let ws = WsConn::new(group_id.into_inner(), data.get_ref().clone());
+	let resp = ws::start(ws, &req, stream);
+	resp.unwrap()
 }
--- project-5/src/ws.rs
+++ project-5/src/ws.rs
@@ -1,65 +1,63 @@
 use crate::lobby::Lobby;
 use crate::message::{ClientActorMessage, Connect, Disconnect, WsMessage};
-use actix::{fut, ActorContext, ActorFuture, ContextFutureSpawner, WrapFuture};
-use actix::{Actor, Addr, StreamHandler, Running};
+use actix::{fut, ActorContext, ActorFutureExt, ContextFutureSpawner, WrapFuture};
+use actix::{Actor, Addr, Running, StreamHandler};
 use actix::{AsyncContext, Handler};
 use actix_web_actors::ws::{self, Message, ProtocolError, WebsocketContext};
 use std::time::{Duration, Instant};
 use uuid::Uuid;
 
-
 const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
 const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
 
-pub struct WsConn{
-	room: Uuid,
-	id: Uuid,
-	lobby_addr: Addr<Lobby>,
-	hb: Instant,
+pub struct WsConn {
+    room: Uuid,
+    id: Uuid,
+    lobby_addr: Addr<Lobby>,
+    hb: Instant,
 }
 
-impl WsConn{
-	pub fn new(room: Uuid, lobby:Addr<Lobby>)-> WsConn{
-		WsConn{
-			room,
-			id: Uuid::new_v4(),
-			lobby_addr: lobby,
-			hb: Instant::now(),
-		}
-	}
+impl WsConn {
+    pub fn new(room: Uuid, lobby: Addr<Lobby>) -> WsConn {
+        WsConn {
+            room,
+            id: Uuid::new_v4(),
+            lobby_addr: lobby,
+            hb: Instant::now(),
+        }
+    }
 }
 
+impl Actor for WsConn {
+    type Context = WebsocketContext<Self>;
 
-impl Actor for WsConn{
-	type Context = WebsocketContext<Self>;
+    fn started(&mut self, ctx: &mut Self::Context) {
+        self.hb(ctx);
+        let addr = ctx.address();
+        self.lobby_addr
+            .send(Connect {
+                addr: addr.recipient(),
+                lobby_id: self.room,
+                self_id: self.id,
+            })
+            .into_actor(self)
+            .then(|res, _act, ctx| {
+                match res {
+                    Ok(_) => (),
+                    _ => ctx.stop(),
+                }
+                fut::ready(())
+            })
+            .wait(ctx);
+    }
 
-	fn started(&mut self, ctx: &mut Self::Context){
-		self.hb(ctx);
-		let addr = ctx.address();
-		self.lobby_addr
-			.send(Connect{
-				addr: addr.recipient(),
-				lobby_id: self.room,
-				self_id: self.id,
-			})
-			.into_actor(self)
-			.then(|res, act, ctx|{
-				match res{
-					Ok(_) => (),
-					_ => ctx.stop(),
-				}
-				fut::ready(())
-			})
-			.wait(ctx);
-	}
-	
-	fn stopping(&mut self, _: &mut Self::Context) -> Running {
-		self.lobby_addr.do_send(Disconnect{
-			id: self.id,
-			room_id: self.room,
-		});
-		Running::Stop
-	}
+    fn stopping(&mut self, _: &mut Self::Context) -> Running {
+        self.lobby_addr.do_send(Disconnect {
+            id: self.id,
+            room_id: self.room,
+        });
+        Running::Stop
+    }
 }
 
 impl WsConn {
@@ -76,34 +74,32 @@ impl WsConn {
     }
 }
 
-impl StreamHandler<Result<Message, ProtocolError>> for WsConn{
-
-	fn handle(&mut self, msg: Result<Message, ProtocolError>, ctx: &mut Self::Context) {
-		match msg{
-			Ok(Message::Ping(msg))=>{
-				self.hb = Instant::now();
-				ctx.pong(&msg);
-			},
-			Ok(Message::Pong(_))=>{
-				self.hb = Instant::now();
-			},
-			Ok(Message::Binary(bin))=>ctx.binary(bin),
-			Ok(Message::Close(reason))=>{
-				ctx.close(reason);
-				ctx.stop();
-			},
-			Ok(Message::Continuation(_))=> ctx.stop(),
-			Ok(Message::Nop)=>(),
-			Ok(Message::Text(s))=> self.lobby_addr.do_send(ClientActorMessage{
-				id: self.id,
-				room_id: self.room,
-				msg: s.to_string(),
-			}),
-			Err(e)=> std::panic::panic_any(e),
-		}
-	}
+impl StreamHandler<Result<Message, ProtocolError>> for WsConn {
+    fn handle(&mut self, msg: Result<Message, ProtocolError>, ctx: &mut Self::Context) {
+        match msg {
+            Ok(Message::Ping(msg)) => {
+                self.hb = Instant::now();
+                ctx.pong(&msg);
+            }
+            Ok(Message::Pong(_)) => {
+                self.hb = Instant::now();
+            }
+            Ok(Message::Binary(bin)) => ctx.binary(bin),
+            Ok(Message::Close(reason)) => {
+                ctx.close(reason);
+                ctx.stop();
+            }
+            Ok(Message::Continuation(_)) => ctx.stop(),
+            Ok(Message::Nop) => (),
+            Ok(Message::Text(s)) => self.lobby_addr.do_send(ClientActorMessage {
+                id: self.id,
+                room_id: self.room,
+                msg: s.to_string(),
+            }),
+            Err(e) => std::panic::panic_any(e)
+        }
+    }
 }
-
 
 impl Handler<WsMessage> for WsConn {
     type Result = ();
